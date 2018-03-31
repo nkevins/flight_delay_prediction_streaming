ac2f22868cff2fd6171179e696d721ad467c7ea9
cloudera <cloudera@quickstart.cloudera>
2018-03-30 22:38:04 -0700

    Filter incoming flight

--- build.sbt
+++ build.sbt
@@ -3,6 +3,9 @@ version := "1.0"
 scalaVersion := "2.11.11"
 
 
+libraryDependencies += "joda-time" % "joda-time" % "2.9.9"
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
 libraryDependencies += "org.apache.spark" %% "spark-streaming" % "2.2.0"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.0"
+libraryDependencies += "org.apache.hadoop" % "hadoop-common" % "2.6.5"
+libraryDependencies += "org.apache.hadoop" % "hadoop-hdfs" % "2.6.5"
--- src/main/scala/sg/edu/iss/stream.scala
+++ src/main/scala/sg/edu/iss/stream.scala
@@ -1,20 +1,30 @@
 package sg.edu.iss
 
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming._
+import org.joda.time.{DateTime, Days, DateTimeZone, Hours, Minutes}
 
 object stream extends App {
   
   val sparkConf = new SparkConf().setAppName("Flight Streaming").setMaster("local[2]")
   val ssc = new StreamingContext(sparkConf, Seconds(5))
   
+  // Load flight today schedule
+  val spark = SparkSession.builder().master("local").appName("Data Processor").getOrCreate()
+  import spark.implicits._
+  val flightScheduleDf = spark.read.json("hdfs://localhost/user/cloudera/cag/" + DateTime.now.plusDays(1).toString("yyyyMMdd") + ".json").select(explode($"carriers").as("flights"))
+  val flightNos = flightScheduleDf.select($"flights.flightNo").rdd.map(r => r(0)).collect()
+  
+  
+  // Start streaming
   val lines = ssc.socketTextStream("localhost", 9008)
   
-  val info = lines.map(_.split(",")).filter(x => x.size >=7 && x(6).startsWith("SQ")).map(_.mkString(","))
+  val filteredLiveTraffic = lines.map(_.split(",")).filter(x => x.size >=7 && flightNos.contains(x(6)) && x(1) != "" && x(2) != "" && x(3) != "")
   
-  info.print()
+  filteredLiveTraffic.map(_.mkString(",")).print()
   
   ssc.start()
   ssc.awaitTermination()
-  
 }
